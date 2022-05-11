ea2ffce7d56a53ba42550819ac0b94c7f4769068
Javier5034 <pj211152@alumno.udb.edu.sv>
2022-05-11 11:02:38 -0600

    Mejoras validaciones crear usuario

--- chevere-master/frmLogIn.Designer.cs
+++ chevere-master/frmLogIn.Designer.cs
@@ -51,33 +51,37 @@
             // 
             // panelImagen
             // 
-            this.panelImagen.Size = new System.Drawing.Size(566, 495);
+            this.panelImagen.Size = new System.Drawing.Size(424, 402);
             // 
             // panel1
             // 
-            this.panel1.Size = new System.Drawing.Size(634, 70);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Size = new System.Drawing.Size(476, 57);
             // 
             // btnClose
             // 
             this.btnClose.FlatAppearance.BorderSize = 0;
-            this.btnClose.Location = new System.Drawing.Point(583, 20);
+            this.btnClose.Location = new System.Drawing.Point(423, 16);
+            this.btnClose.Padding = new System.Windows.Forms.Padding(6, 0, 11, 0);
             // 
             // btnMaximizise
             // 
             this.btnMaximizise.FlatAppearance.BorderSize = 0;
-            this.btnMaximizise.Location = new System.Drawing.Point(531, 20);
+            this.btnMaximizise.Location = new System.Drawing.Point(381, 15);
+            this.btnMaximizise.Padding = new System.Windows.Forms.Padding(6, 0, 11, 0);
             // 
             // btnMinimize
             // 
             this.btnMinimize.FlatAppearance.BorderSize = 0;
-            this.btnMinimize.Location = new System.Drawing.Point(479, 20);
+            this.btnMinimize.Location = new System.Drawing.Point(339, 16);
+            this.btnMinimize.Padding = new System.Windows.Forms.Padding(6, 0, 11, 0);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = global::chevere_master.Properties.Resources.imagen1;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Size = new System.Drawing.Size(566, 495);
+            this.pictureBox1.Size = new System.Drawing.Size(424, 402);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             // 
             // panel2
@@ -92,7 +96,7 @@
             this.panel2.Controls.Add(this.txtUsuario);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Size = new System.Drawing.Size(634, 425);
+            this.panel2.Size = new System.Drawing.Size(476, 345);
             this.panel2.Controls.SetChildIndex(this.btnSiguiente, 0);
             this.panel2.Controls.SetChildIndex(this.btnAtras, 0);
             this.panel2.Controls.SetChildIndex(this.label1, 0);
@@ -108,29 +112,32 @@
             // 
             // lblTitleChildForm
             // 
-            this.lblTitleChildForm.Location = new System.Drawing.Point(93, 14);
-            this.lblTitleChildForm.Size = new System.Drawing.Size(163, 38);
+            this.lblTitleChildForm.Location = new System.Drawing.Point(70, 11);
+            this.lblTitleChildForm.Size = new System.Drawing.Size(131, 30);
             this.lblTitleChildForm.Text = "Welcome!";
             // 
             // IconCurrentChildForm
             // 
             this.IconCurrentChildForm.IconChar = FontAwesome.Sharp.IconChar.Route;
-            this.IconCurrentChildForm.Location = new System.Drawing.Point(28, 12);
+            this.IconCurrentChildForm.IconSize = 33;
+            this.IconCurrentChildForm.Location = new System.Drawing.Point(21, 10);
+            this.IconCurrentChildForm.Size = new System.Drawing.Size(33, 34);
             // 
             // btnHome
             // 
-            this.btnHome.Location = new System.Drawing.Point(268, 3);
+            this.btnHome.Location = new System.Drawing.Point(222, 2);
             // 
             // btnAtras
             // 
-            this.btnAtras.Location = new System.Drawing.Point(-202, 189);
+            this.btnAtras.Location = new System.Drawing.Point(-193, 76);
             this.btnAtras.Text = "Salir";
             this.btnAtras.Visible = false;
             // 
             // btnSiguiente
             // 
             this.btnSiguiente.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.btnSiguiente.Location = new System.Drawing.Point(243, 257);
+            this.btnSiguiente.Location = new System.Drawing.Point(186, 220);
+            this.btnSiguiente.Size = new System.Drawing.Size(77, 28);
             this.btnSiguiente.Text = "Log in";
             this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
             // 
@@ -139,9 +146,10 @@
             this.linkLabel2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.linkLabel2.AutoSize = true;
             this.linkLabel2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel2.Location = new System.Drawing.Point(176, 332);
+            this.linkLabel2.Location = new System.Drawing.Point(133, 269);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(253, 17);
+            this.linkLabel2.Size = new System.Drawing.Size(215, 16);
             this.linkLabel2.TabIndex = 37;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "Aún sin cuenta?Crea tu cuenta ahora";
@@ -152,9 +160,10 @@
             this.linklblPassBack.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.linklblPassBack.AutoSize = true;
             this.linklblPassBack.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linklblPassBack.Location = new System.Drawing.Point(215, 309);
+            this.linklblPassBack.Location = new System.Drawing.Point(162, 250);
+            this.linklblPassBack.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linklblPassBack.Name = "linklblPassBack";
-            this.linklblPassBack.Size = new System.Drawing.Size(154, 17);
+            this.linklblPassBack.Size = new System.Drawing.Size(132, 16);
             this.linklblPassBack.TabIndex = 36;
             this.linklblPassBack.TabStop = true;
             this.linklblPassBack.Text = "Recuperar Contraseña";
@@ -164,10 +173,10 @@
             // 
             this.btnVerContrasenia.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnVerContrasenia.Image = global::chevere_master.Properties.Resources._495_4950508_show_password_show_password_icon_png_transparent_png;
-            this.btnVerContrasenia.Location = new System.Drawing.Point(445, 214);
-            this.btnVerContrasenia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnVerContrasenia.Location = new System.Drawing.Point(334, 173);
+            this.btnVerContrasenia.Margin = new System.Windows.Forms.Padding(2);
             this.btnVerContrasenia.Name = "btnVerContrasenia";
-            this.btnVerContrasenia.Size = new System.Drawing.Size(27, 22);
+            this.btnVerContrasenia.Size = new System.Drawing.Size(20, 18);
             this.btnVerContrasenia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.btnVerContrasenia.TabIndex = 35;
             this.btnVerContrasenia.TabStop = false;
@@ -177,28 +186,29 @@
             // 
             this.txtMessage.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtMessage.AutoSize = true;
-            this.txtMessage.Location = new System.Drawing.Point(176, 257);
+            this.txtMessage.Location = new System.Drawing.Point(133, 208);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(0, 16);
+            this.txtMessage.Size = new System.Drawing.Size(0, 13);
             this.txtMessage.TabIndex = 34;
             // 
             // txtPass
             // 
             this.txtPass.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtPass.Location = new System.Drawing.Point(176, 214);
-            this.txtPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPass.Location = new System.Drawing.Point(133, 173);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(249, 22);
+            this.txtPass.Size = new System.Drawing.Size(188, 20);
             this.txtPass.TabIndex = 31;
             this.txtPass.UseSystemPasswordChar = true;
             // 
             // txtUsuario
             // 
             this.txtUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtUsuario.Location = new System.Drawing.Point(176, 161);
-            this.txtUsuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtUsuario.Location = new System.Drawing.Point(133, 130);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(249, 22);
+            this.txtUsuario.Size = new System.Drawing.Size(188, 20);
             this.txtUsuario.TabIndex = 30;
             // 
             // label3
@@ -206,9 +216,10 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(120, 114);
+            this.label3.Location = new System.Drawing.Point(91, 92);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(394, 19);
+            this.label3.Size = new System.Drawing.Size(321, 17);
             this.label3.TabIndex = 29;
             this.label3.Text = "¿Tienes pensado un nuevo sitio para conocer?";
             // 
@@ -217,9 +228,10 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(117, 59);
+            this.label1.Location = new System.Drawing.Point(88, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(269, 39);
+            this.label1.Size = new System.Drawing.Size(222, 32);
             this.label1.TabIndex = 28;
             this.label1.Text = "¡Hola de nuevo!";
             // 
@@ -230,9 +242,11 @@
             this.ipicpass.IconChar = FontAwesome.Sharp.IconChar.Lock;
             this.ipicpass.IconColor = System.Drawing.SystemColors.ControlText;
             this.ipicpass.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.ipicpass.Location = new System.Drawing.Point(124, 214);
+            this.ipicpass.IconSize = 24;
+            this.ipicpass.Location = new System.Drawing.Point(93, 174);
+            this.ipicpass.Margin = new System.Windows.Forms.Padding(2);
             this.ipicpass.Name = "ipicpass";
-            this.ipicpass.Size = new System.Drawing.Size(32, 32);
+            this.ipicpass.Size = new System.Drawing.Size(24, 26);
             this.ipicpass.TabIndex = 38;
             this.ipicpass.TabStop = false;
             // 
@@ -243,18 +257,20 @@
             this.ipicuser.IconChar = FontAwesome.Sharp.IconChar.UserAlt;
             this.ipicuser.IconColor = System.Drawing.SystemColors.ControlText;
             this.ipicuser.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.ipicuser.Location = new System.Drawing.Point(124, 161);
+            this.ipicuser.IconSize = 24;
+            this.ipicuser.Location = new System.Drawing.Point(93, 131);
+            this.ipicuser.Margin = new System.Windows.Forms.Padding(2);
             this.ipicuser.Name = "ipicuser";
-            this.ipicuser.Size = new System.Drawing.Size(32, 32);
+            this.ipicuser.Size = new System.Drawing.Size(24, 26);
             this.ipicuser.TabIndex = 39;
             this.ipicuser.TabStop = false;
             // 
             // frmLogIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1200, 495);
+            this.ClientSize = new System.Drawing.Size(900, 402);
             this.Name = "frmLogIn";
             this.panelImagen.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
